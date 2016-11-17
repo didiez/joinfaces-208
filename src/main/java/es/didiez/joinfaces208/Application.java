45e1877e8301041835a4b191cbe7cfa8c2b42a5d
didiez <didiez@localhost>
2016-11-17 10:26:45 +0100

    Sample project to test https://github.com/joinfaces/joinfaces/issues/208

---
+++ src/main/java/es/didiez/joinfaces208/Application.java
@@ -0,0 +1,16 @@
+package es.didiez.joinfaces208;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author didiez
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
